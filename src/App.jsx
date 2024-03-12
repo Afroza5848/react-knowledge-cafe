d41fb378e9b5eced7e372718d5bff98006a865b7
Afroza5848 <suriyaafroj5848@gmail.com>
2024-03-13 05:59:04 +0600

    react-knowledge-cafe projects create start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+      <h1 className='text-2xl text-red-500 bg-slate-500'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
